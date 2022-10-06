62cf314c0b0348ff0e64f172c88992016836d981
Zineddine <zineddine.aliche.01@gmail.com>
2022-10-06 16:27:07 -0400

    Key pair_ security grpi

--- aws_ec2.py
+++ aws_ec2.py
@@ -1,5 +1,8 @@
 # Create EC2 instances using Python BOTO3
 import boto3
+import keypair_securitygroup as confi
+
+confi.key_pair()
 
 """
 def create_key_pair():
@@ -16,14 +19,15 @@ create_key_pair()
 """
 availabilityZone = 'us-east-1b'
 resource_ec2 = boto3.resource('ec2')
+data_instances = []
 def create_ec2_instance():
         print("Creating EC2 instance")
         resource_ec2 = boto3.client('ec2')
-        resource_ec2.describe_instances()
+        """
         resource_ec2.run_instances(
             ImageId="ami-0f1ee03d06c4c659c",
             MinCount=1,
-            MaxCount=1,
+            MaxCount=2,
             InstanceType="m4.large",
             KeyName="vockey",
             Placement={
@@ -31,14 +35,31 @@ def create_ec2_instance():
             }
 
         )
-        instanceIDs = resource_ec2.describe_instances()
-        print(instanceIDs)
-        return instanceIDs
+        """
 
-create_ec2_instance()
+        instanceIDs = resource_ec2.describe_instances()
+
+        print(classic_address_iterator)
+
+        return instanceIDs
+classic_address_iterator = resource_ec2.classic_addresses.all()
+instanceIDs = create_ec2_instance()
+#print(instanceIDs.keys())
+data = instanceIDs["Reservations"][0]
+#print('data', data)
+data1 = data["Instances"]
+#print('data1', data1)
+#print(len(data1))
+for i in range(len(data1)):
+    print(f"Istances Id is :{data1[i]['InstanceId']}")
+    data_instances.append(data1[i]['InstanceId'])
+print(data_instances)
+
+
 # Terminate instances
 #instanceIDs = ['i-05687a9ebbfbe2856']
 def terminate(instanceIDs):
     print('Terminate instance...')
     resource_ec2.instances.filter(InstanceIds=instanceIDs).terminate()
 #terminate(instanceIDs)
+
