96dd31e19c80d8f75a2741948d7d3e0baf163473
LineG <ghanemline@gmail.com>
2022-10-09 18:46:52 +0200

    app deployment using flask

---
+++ tmp/deploy_flask_app.py
@@ -0,0 +1,55 @@
+import helper_methods
+import paramiko
+import time
+
+
+def ssh_connect_with_retry(ssh, ip_address, retries):
+    if retries > 3:
+        return False
+    privkey = paramiko.RSAKey.from_private_key_file(
+        'labsuser.pem')
+    interval = 5
+    try:
+        retries += 1
+        print('SSH into the instance: {}'.format(ip_address))
+        ssh.connect(hostname=ip_address,
+                    username='ubuntu', pkey=privkey)
+        return True
+    except Exception as e:
+        print(e)
+        time.sleep(interval)
+        print('Retrying SSH connection to {}'.format(ip_address))
+        ssh_connect_with_retry(ssh, ip_address, retries)
+
+
+bash = """
+#!/bin/bash
+yes | sudo apt-get update
+yes | sudo apt install python3-pip
+yes | sudo apt install python3.10-venv
+mkdir flask_application && cd flask_application
+sudo python3 -m venv venv
+sudo source venv/bin/activate
+yes | sudo pip install Flask
+cat <<EOF > app.py
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def my_app():
+    return "Your small app is working"
+EOF
+export flask_application=app.py
+nohup sudo flask run --host=0.0.0.0 --port=80 1>/dev/null 2>/dev/null &
+echo hola que tal
+"""
+
+
+def deploy_and_setup_app():
+    running_instances = helper_methods.get_running_instances()
+    ip_address = running_instances[0][1]
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_connect_with_retry(ssh, ip_address, 0)
+    stdin, stdout, stderr = ssh.exec_command(bash)
+    print('stdout:', stdout.read())
+    # print('stderr:', stderr.read())
---
+++ tmp/set_up.py
@@ -0,0 +1,13 @@
+import set_up_instances
+import deploy_flask_app
+import time
+
+# IMPORTANT before running the file download your private key
+
+# Setting the instances
+print("Let set up the instances")
+set_up_instances.run_instances()
+time.sleep(30)
+# Deploying flask in all of the running instances
+print("Deplying the flask app")
+deploy_flask_app.deploy_and_setup_app()
