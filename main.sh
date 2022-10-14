3202a1cb1e5254807501b3e7b87b1de76c7be621
rayaneDZ <chlef-algerie@hotmail.fr>
2022-10-14 11:16:40 -0400

    refactored the structure of the repo and added requirements.txt

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Installing the following dependencies: boto3 - requests - paramiko"
-pip install boto3
-pip install requests
-pip install paramiko
+pip install boto3==1.24.89
+pip install botocore==1.27.89
+pip install paramiko==2.11.0
+pip install requests==2.22.0
 
 echo "Clonning the git repo to proceed with the deployment"
 git clone https://github.com/axellepgr/lab1_log8415.git
@@ -12,5 +13,5 @@ read -p "Was the previous step completed?(y)" confirm && [[ $confirm == [yY] || 
 echo "2-Please enter the .pem file of your key pair names 'vockey' in the root folder of lab1_log8415"
 read -p "Was the previous step completed?(y)" confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
-cd lab1_log8415/tmp
-python set_up.py
+cd lab1_log8415/src
+python launch.py
