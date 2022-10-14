dd832ec0fa071c9f5d9e005eabb3a2260d7f5a5e
rayaneDZ <chlef-algerie@hotmail.fr>
2022-10-14 16:06:36 -0400

    added functionalities to the ploting script

--- src/plotgraphs.py
+++ src/plotgraphs.py
@@ -7,26 +7,39 @@ with open('metrics.json', 'r') as openfile:
     openfile.close()
 
 
-def plot_graphs_for_instances(type, metric):
-    m4_instances_CPU = metrics["m4_instances"]["CPU_Utilization"]
-    print(m4_instances_CPU)
-    for instance in m4_instances_CPU:
-        x = []
-        y = []
-        id = ""
-        for key in instance:
-            id = key
-        for point in instance[id]:
-            x.append(point[0])
-            y.append(point[1])
-        plt.figure()
-        plt.plot(x, y)
-        plt.xlabel('timestamp')
-        plt.ylabel('Percent')
-        if metric == "NetworkIn":
-            plt.ylabel('Bytes')
-        plt.title(id)
+def plot_graphs_for_instances(type):
+
+    # json object containing metrics
+    m4_instances_CPU = metrics[type]
+
+    # metric is a list containing json objects (on object for each instance)
+    for metric in m4_instances_CPU:
+
+        # instance is a json object with one key (instanceID) and its value is the datapoints
+        for instance in m4_instances_CPU[metric]:
+            x = []
+            y = []
+
+            # retreive the id of the instance
+            id = ""
+            for key in instance:
+                id = key
+            for point in instance[id]:
+                point[0] = point[0].split()[1]
+                x.append(point[0])
+                y.append(point[1])
+            plt.figure(id)
+            if metric == "CPU_Utilization":
+                plt.subplot(211)
+            elif metric == "NetworkIn":
+                plt.subplot(212)
+            plt.plot(x, y, label=metric)
+            plt.xlabel('timestamp')
+            plt.ylabel('Percent')
+            if metric == "NetworkIn":
+                plt.ylabel('Bytes')
+            plt.title(id)
     plt.show()
 
 
-plot_graphs_for_instances("m4_instances", 'CPUUtilization')
+plot_graphs_for_instances("m4_instances")
